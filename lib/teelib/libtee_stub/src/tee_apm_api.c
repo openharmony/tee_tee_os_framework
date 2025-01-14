ee1eec9eae97c6cec4b5de6c828f3848cb55ab73
suwanghw <wangsu14@huawei.com>
2025-01-14 23:16:00 +0800

    warning fix
    Signed-off-by: suwanghw <wangsu14@huawei.com>

--- lib/teelib/libtee_stub/src/tee_apm_api.c
+++ lib/teelib/libtee_stub/src/tee_apm_api.c
@@ -1,3 +1,14 @@
+/*
+ * Copyright (C) 2022 Huawei Technologies Co., Ltd.
+ * Licensed under the Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *     http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
 #include <tee_apm_api.h>
 
 TEE_Result tee_query_ta_measure_report(const TEE_UUID *uuid, struct ta_measure_report_t *report)
